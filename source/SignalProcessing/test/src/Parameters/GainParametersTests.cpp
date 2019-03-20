c4c1498f678ba28296afdfd8a6dc6cf608a0dca4
Marc-Antoine <maheux.marcantoine@gmail.com>
2019-03-20 18:51:35 -0400

    Clean up + ajout des classes pour les paramètres de mixage et de gain.

---
+++ source/SignalProcessing/test/src/Parameters/GainParametersTests.cpp
@@ -0,0 +1,29 @@
+#include <SignalProcessing/Parameters/GainParameters.h>
+
+#include <gtest/gtest.h>
+
+using namespace adaptone;
+using namespace std;
+
+TEST(GainParametersTests, constructor_shouldInitializeAllGainsTo1)
+{
+    GainParameters<float> gainParameters(2);
+
+    EXPECT_EQ(gainParameters.gains(), vector<float>({ 1, 1 }));
+}
+
+TEST(GainParametersTests, setGain_invalidChannel_shouldThrowInvalidException)
+{
+    GainParameters<float> gainParameters(2);
+
+    EXPECT_THROW(gainParameters.setGain(2, 20), InvalidValueException);
+}
+
+TEST(GainParametersTests, setGain_shouldSetSpecifiedChannelGainNotInDb)
+{
+    GainParameters<float> gainParameters(2);
+
+    gainParameters.setGain(0, 20);
+
+    EXPECT_EQ(gainParameters.gains(), vector<float>({ 10, 1 }));
+}
