5c2bac9c2b86acd599f0c3d6656a811e4ca98ce6
David Dallaire <ddallaire25@gmail.com>
2019-04-09 15:01:28 -0400

    Convert array to pcm data (#27)
    * Convert arrays to pcm data
    
    * Correction des functions pointeurs erronés
    
    * Test de la saturation
    
    * Ajout d'un test en double
    
    * Review comments

---
+++ source/SignalProcessing/include/SignalProcessing/Cuda/Conversion/ArrayToPcmConversion.h
@@ -0,0 +1,272 @@
+#ifndef SIGNAL_PROCESSING_CUDA_CONVERSION_ARRAY_TO_PCM_CONVERSION_H
+#define SIGNAL_PROCESSING_CUDA_CONVERSION_ARRAY_TO_PCM_CONVERSION_H
+
+#include <Utils/Data/PcmAudioFrame.h>
+#include <Utils/TypeTraits.h>
+
+#include <cstddef>
+#include <cstdint>
+#include <limits>
+
+namespace adaptone
+{
+    template<class T>
+    inline __device__ T round(T value)
+    {
+        static_assert(AlwaysFalse<T>::value, "Not supported");
+    }
+
+    template<>
+    inline __device__ float round(float value)
+    {
+        return roundf(value);
+    }
+
+    template<>
+    inline __device__ double round(double value)
+    {
+        return nearbyint(value);
+    }
+
+    template<class T>
+    inline __device__ T saturateOutput(T value, T min, T max)
+    {
+        if (value > max)
+        {
+            return max;
+        }
+        if (value < min)
+        {
+            return min;
+        }
+
+        return value;
+    }
+
+    template<>
+    inline __device__ float saturateOutput(float value, float min, float max)
+    {
+        return __saturatef((value - min) / (max - min)) * (max - min) + min;
+    }
+
+    template<class T, class PcmT>
+    __device__ void arrayToSignedPcm(const T* input, uint8_t* outputBytes, std::size_t frameSampleCount,
+        std::size_t channelCount)
+    {
+        std::size_t startIndex = threadIdx.x;
+        std::size_t stride = blockDim.x;
+        std::size_t n = frameSampleCount * channelCount;
+
+        PcmT* output = reinterpret_cast<PcmT*>(outputBytes);
+
+        for (std::size_t i = startIndex; i < n; i += stride)
+        {
+            std::size_t channelIndex = i % channelCount;
+            std::size_t sampleIndex = i / channelCount;
+
+            T scaledSample = -input[channelIndex * frameSampleCount + sampleIndex] * std::numeric_limits<PcmT>::min();
+            T sample = saturateOutput(scaledSample,
+                static_cast<T>(std::numeric_limits<PcmT>::min()),
+                static_cast<T>(std::numeric_limits<PcmT>::max()));
+            output[i] = static_cast<PcmT>(round(sample));
+        }
+    };
+
+    template<class T>
+    __device__ void arrayToSigned24Pcm(const T* input, uint8_t* output, std::size_t frameSampleCount,
+        std::size_t channelCount)
+    {
+        constexpr T AbsMin = 1 << 23;
+
+        std::size_t startIndex = threadIdx.x;
+        std::size_t stride = blockDim.x;
+        std::size_t n = frameSampleCount * channelCount;
+
+        for (std::size_t i = startIndex; i < n; i += stride)
+        {
+            std::size_t channelIndex = i % channelCount;
+            std::size_t sampleIndex = i / channelCount;
+
+            T scaledSample = input[channelIndex * frameSampleCount + sampleIndex] * AbsMin;
+            T sample = saturateOutput(scaledSample, -AbsMin, AbsMin - 1);
+
+            int32_t sampleInteger = static_cast<int32_t>(round(sample));
+            uint32_t b0 = sampleInteger & 0xff;
+            uint32_t b1 = (sampleInteger >> 8) & 0xff;
+            uint32_t b2 = (sampleInteger >> 16) & 0xff;
+
+            output[3 * i] = static_cast<uint8_t>(b0);
+            output[3 * i + 1] = static_cast<uint8_t>(b1);
+            output[3 * i + 2] = static_cast<uint8_t>(b2);
+        }
+    }
+
+    template<class T>
+    __device__ void arrayToSignedPadded24Pcm(const T* input, uint8_t* outputBytes, std::size_t frameSampleCount,
+        std::size_t channelCount)
+    {
+        constexpr T AbsMin = 1 << 23;
+
+        std::size_t startIndex = threadIdx.x;
+        std::size_t stride = blockDim.x;
+        std::size_t n = frameSampleCount * channelCount;
+
+        int32_t* output = reinterpret_cast<int32_t*>(outputBytes);
+
+        for (std::size_t i = startIndex; i < n; i += stride)
+        {
+            std::size_t channelIndex = i % channelCount;
+            std::size_t sampleIndex = i / channelCount;
+
+            T scaledSample = input[channelIndex * frameSampleCount + sampleIndex] * AbsMin;
+            T sample = saturateOutput(scaledSample, -AbsMin, AbsMin - 1);
+            output[i] = static_cast<int32_t>(round(sample));
+        }
+    };
+
+    template<class T, class PcmT>
+    __device__ void arrayToUnsignedPcm(const T* input, uint8_t* outputBytes, std::size_t frameSampleCount,
+        std::size_t channelCount)
+    {
+        std::size_t startIndex = threadIdx.x;
+        std::size_t stride = blockDim.x;
+        std::size_t n = frameSampleCount * channelCount;
+
+        PcmT* output = reinterpret_cast<PcmT*>(outputBytes);
+
+        for (std::size_t i = startIndex; i < n; i += stride)
+        {
+            std::size_t channelIndex = i % channelCount;
+            std::size_t sampleIndex = i / channelCount;
+
+            T scaledSample = (input[channelIndex * frameSampleCount + sampleIndex] + 1) / 2 *
+                std::numeric_limits<PcmT>::max();
+            T sample = saturateOutput(scaledSample,
+                static_cast<T>(std::numeric_limits<PcmT>::min()),
+                static_cast<T>(std::numeric_limits<PcmT>::max()));
+            output[i] = static_cast<PcmT>(round(sample));
+        }
+    };
+
+    template<class T>
+    __device__ void arrayToUnsigned24Pcm(const T* input, uint8_t* output, std::size_t frameSampleCount,
+        std::size_t channelCount)
+    {
+        constexpr T Max = (1 << 24) - 1;
+
+        std::size_t startIndex = threadIdx.x;
+        std::size_t stride = blockDim.x;
+        std::size_t n = frameSampleCount * channelCount;
+
+        for (std::size_t i = startIndex; i < n; i += stride)
+        {
+            std::size_t channelIndex = i % channelCount;
+            std::size_t sampleIndex = i / channelCount;
+
+            T scaledSample = (input[channelIndex * frameSampleCount + sampleIndex] + 1) / 2 * Max;
+            T sample = saturateOutput(scaledSample, static_cast<T>(0), Max);
+
+            uint32_t sampleInteger = static_cast<uint32_t>(round(sample));
+            uint32_t b0 = sampleInteger & 0xff;
+            uint32_t b1 = (sampleInteger >> 8) & 0xff;
+            uint32_t b2 = (sampleInteger >> 16) & 0xff;
+
+            output[3 * i] = static_cast<uint8_t>(b0);
+            output[3 * i + 1] = static_cast<uint8_t>(b1);
+            output[3 * i + 2] = static_cast<uint8_t>(b2);
+        }
+    }
+
+    template<class T>
+    __device__ void arrayToUnsignedPadded24Pcm(const T* input, uint8_t* outputBytes, std::size_t frameSampleCount,
+        std::size_t channelCount)
+    {
+        constexpr T Max = (1 << 24) - 1;
+
+        std::size_t startIndex = threadIdx.x;
+        std::size_t stride = blockDim.x;
+        std::size_t n = frameSampleCount * channelCount;
+
+        uint32_t* output = reinterpret_cast<uint32_t*>(outputBytes);
+
+        for (std::size_t i = startIndex; i < n; i += stride)
+        {
+            std::size_t channelIndex = i % channelCount;
+            std::size_t sampleIndex = i / channelCount;
+
+            T scaledSample = (input[channelIndex * frameSampleCount + sampleIndex] + 1) / 2 * Max;
+            T sample = saturateOutput(scaledSample, static_cast<T>(0), Max);
+            output[i] = static_cast<uint32_t>(round(sample));
+        }
+    }
+
+    template<class T, class PcmT>
+    __device__ void arrayToFloatingPointPcm(const T* input, uint8_t* outputBytes, std::size_t frameSampleCount,
+        std::size_t channelCount)
+    {
+        std::size_t startIndex = threadIdx.x;
+        std::size_t stride = blockDim.x;
+        std::size_t n = frameSampleCount * channelCount;
+
+        PcmT* output = reinterpret_cast<PcmT*>(outputBytes);
+
+        for (std::size_t i = startIndex; i < n; i += stride)
+        {
+            std::size_t channelIndex = i % channelCount;
+            std::size_t sampleIndex = i / channelCount;
+
+            T sample = saturateOutput(input[channelIndex * frameSampleCount + sampleIndex], static_cast<T>(-1),
+                static_cast<T>(1));
+            output[i] = static_cast<PcmT>(sample);
+        }
+    };
+
+    template<class T>
+    __device__ void convertArrayToPcm(const T* input, uint8_t* outputBytes, std::size_t frameSampleCount,
+        std::size_t channelCount, PcmAudioFrame::Format format)
+    {
+        switch (format)
+        {
+            case PcmAudioFrame::Format::Signed8:
+                arrayToSignedPcm<T, int8_t>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+            case PcmAudioFrame::Format::Signed16:
+                arrayToSignedPcm<T, int16_t>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+            case PcmAudioFrame::Format::Signed24:
+                arrayToSigned24Pcm<T>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+            case PcmAudioFrame::Format::SignedPadded24:
+                arrayToSignedPadded24Pcm<T>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+            case PcmAudioFrame::Format::Signed32:
+                arrayToSignedPcm<T, int32_t>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+
+            case PcmAudioFrame::Format::Unsigned8:
+                arrayToUnsignedPcm<T, uint8_t>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+            case PcmAudioFrame::Format::Unsigned16:
+                arrayToUnsignedPcm<T, uint16_t>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+            case PcmAudioFrame::Format::Unsigned24:
+                arrayToUnsigned24Pcm<T>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+            case PcmAudioFrame::Format::UnsignedPadded24:
+                arrayToUnsignedPadded24Pcm<T>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+            case PcmAudioFrame::Format::Unsigned32:
+                arrayToUnsignedPcm<T, uint32_t>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+
+            case PcmAudioFrame::Format::Float:
+                arrayToFloatingPointPcm<T, float>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+            case PcmAudioFrame::Format::Double:
+                arrayToFloatingPointPcm<T, double>(input, outputBytes, frameSampleCount, channelCount);
+                break;
+        }
+    }
+}
+
+#endif
--- source/SignalProcessing/include/SignalProcessing/Cuda/Conversion/PcmToArrayConversion.h
+++ source/SignalProcessing/include/SignalProcessing/Cuda/Conversion/PcmToArrayConversion.h
@@ -2,7 +2,6 @@
 #define SIGNAL_PROCESSING_CUDA_CONVERSION_PCM_TO_ARRAY_CONVERSION_H
 
 #include <Utils/Data/PcmAudioFrame.h>
-#include <Utils/Exception/NotSupportedException.h>
 
 #include <cstddef>
 #include <cstdint>
@@ -25,7 +24,7 @@ namespace adaptone
             std::size_t channelIndex = i % channelCount;
             std::size_t sampleIndex = i / channelCount;
 
-            T sample = -static_cast<T>(input[i]) / std::numeric_limits<PcmT>::min();
+            T sample = static_cast<T>(input[i]) / std::numeric_limits<PcmT>::max();
             output[channelIndex * frameSampleCount + sampleIndex] = sample;
         }
     };
@@ -164,115 +163,49 @@ namespace adaptone
     };
 
     template<class T>
-    using PcmToArrayConversionFunctionPointer = void (*)(const uint8_t*, T*, std::size_t, std::size_t);
-
-    template<class T, class PcmT>
-    __global__ void getSignedPcmToArrayAddress(PcmToArrayConversionFunctionPointer<T>* pointer)
+    __device__ void convertPcmToArray(const uint8_t* inputBytes, T* output, std::size_t frameSampleCount,
+        std::size_t channelCount, PcmAudioFrame::Format format)
     {
-        *pointer = signedPcmToArray<T, PcmT>;
-    }
-
-    template<class T>
-    __global__ void  getSigned24PcmToArrayAddress(PcmToArrayConversionFunctionPointer<T>* pointer)
-    {
-        *pointer = signed24PcmToArray<T>;
-    }
-
-    template<class T>
-    __global__ void getSignedPadded24PcmToArrayAddress(PcmToArrayConversionFunctionPointer<T>* pointer)
-    {
-        *pointer = signedPadded24PcmToArray<T>;
-    }
-
-    template<class T, class PcmT>
-    __global__ void getUnsignedPcmToArrayAddress(PcmToArrayConversionFunctionPointer<T>* pointer)
-    {
-        *pointer = unsignedPcmToArray<T, PcmT>;
-    }
-
-    template<class T>
-    __global__ void getUnsigned24PcmToArrayAddress(PcmToArrayConversionFunctionPointer<T>* pointer)
-    {
-        *pointer = unsigned24PcmToArray<T>;
-    }
-
-    template<class T>
-    __global__ void getUnsignedPadded24PcmToArrayAddress(PcmToArrayConversionFunctionPointer<T>* pointer)
-    {
-        *pointer = unsignedPadded24PcmToArray<T>;
-    }
-
-    template<class T, class PcmT>
-    __global__ void getFloatingPointPcmToArrayAddress(PcmToArrayConversionFunctionPointer<T>* pointer)
-    {
-        *pointer = floatingPointPcmToArray<T, PcmT>;
-    }
-
-    template<class T>
-    PcmToArrayConversionFunctionPointer<T> getPcmToArrayConversionFunctionPointer(PcmAudioFrame::Format format)
-    {
-        PcmToArrayConversionFunctionPointer<T> gpuFunctionPointer;
-        PcmToArrayConversionFunctionPointer<T>* d_gpuFunctionPointer;
-        cudaMalloc(&d_gpuFunctionPointer, sizeof(PcmToArrayConversionFunctionPointer<T>*));
-
-        bool supported = true;
         switch (format)
         {
             case PcmAudioFrame::Format::Signed8:
-                getSignedPcmToArrayAddress<T, int8_t><<<1, 1>>>(d_gpuFunctionPointer);
+                signedPcmToArray<T, int8_t>(inputBytes, output, frameSampleCount, channelCount);
                 break;
             case PcmAudioFrame::Format::Signed16:
-                getSignedPcmToArrayAddress<T, int16_t><<<1, 1>>>(d_gpuFunctionPointer);
+                signedPcmToArray<T, int16_t>(inputBytes, output, frameSampleCount, channelCount);
                 break;
             case PcmAudioFrame::Format::Signed24:
-                getSigned24PcmToArrayAddress<T><<< 1, 1 >>>(d_gpuFunctionPointer);
+                signed24PcmToArray<T>(inputBytes, output, frameSampleCount, channelCount);
                 break;
             case PcmAudioFrame::Format::SignedPadded24:
-                getSignedPadded24PcmToArrayAddress<T><<<1, 1>>>(d_gpuFunctionPointer);
+                signedPadded24PcmToArray<T>(inputBytes, output, frameSampleCount, channelCount);
                 break;
             case PcmAudioFrame::Format::Signed32:
-                getSignedPcmToArrayAddress<T, int32_t><<<1, 1>>>(d_gpuFunctionPointer);
+                signedPcmToArray<T, int32_t>(inputBytes, output, frameSampleCount, channelCount);
                 break;
 
             case PcmAudioFrame::Format::Unsigned8:
-                getUnsignedPcmToArrayAddress<T, uint8_t> <<<1, 1>>>(d_gpuFunctionPointer);
+                unsignedPcmToArray<T, uint8_t>(inputBytes, output, frameSampleCount, channelCount);
                 break;
             case PcmAudioFrame::Format::Unsigned16:
-                getUnsignedPcmToArrayAddress<T, uint16_t><<<1, 1>>>(d_gpuFunctionPointer);
+                unsignedPcmToArray<T, uint16_t>(inputBytes, output, frameSampleCount, channelCount);
                 break;
             case PcmAudioFrame::Format::Unsigned24:
-                getUnsigned24PcmToArrayAddress<T><<<1, 1>>>(d_gpuFunctionPointer);
+                unsigned24PcmToArray<T>(inputBytes, output, frameSampleCount, channelCount);
                 break;
             case PcmAudioFrame::Format::UnsignedPadded24:
-                getUnsignedPadded24PcmToArrayAddress<T> <<<1, 1>>>(d_gpuFunctionPointer);
+                unsignedPadded24PcmToArray<T>(inputBytes, output, frameSampleCount, channelCount);
                 break;
             case PcmAudioFrame::Format::Unsigned32:
-                getUnsignedPcmToArrayAddress<T, uint32_t><<<1, 1>>>(d_gpuFunctionPointer);
+                unsignedPcmToArray<T, uint32_t>(inputBytes, output, frameSampleCount, channelCount);
                 break;
 
             case PcmAudioFrame::Format::Float:
-                getFloatingPointPcmToArrayAddress<T, float><<<1, 1>>>(d_gpuFunctionPointer);
+                floatingPointPcmToArray<T, float>(inputBytes, output, frameSampleCount, channelCount);
                 break;
             case PcmAudioFrame::Format::Double:
-                getFloatingPointPcmToArrayAddress<T, double><<<1, 1>>>(d_gpuFunctionPointer);
+                floatingPointPcmToArray<T, double>(inputBytes, output, frameSampleCount, channelCount);
                 break;
-
-            default:
-                supported = false;
-                break;
-        }
-
-        cudaMemcpy(&gpuFunctionPointer, d_gpuFunctionPointer, sizeof(PcmToArrayConversionFunctionPointer<T>*),
-            cudaMemcpyDeviceToHost);
-        cudaFree(d_gpuFunctionPointer);
-
-        if (supported)
-        {
-            return gpuFunctionPointer;
-        }
-        else
-        {
-            THROW_NOT_SUPPORTED_EXCEPTION("Not supported pcm format.");
         }
     }
 }
