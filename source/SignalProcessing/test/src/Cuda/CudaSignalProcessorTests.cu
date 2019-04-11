882b06253b991112478ab839b6970de0c5a94f3f
ddallaire <ddallaire25@gmail.com>
2019-04-11 08:44:47 -0400

    Format list of frequencies

--- source/SignalProcessing/test/src/Cuda/CudaSignalProcessorTests.cu
+++ source/SignalProcessing/test/src/Cuda/CudaSignalProcessorTests.cu
@@ -17,7 +17,8 @@ TEST(CudaSignalProcessorTests, performance)
     constexpr PcmAudioFrame::Format inputFormat = PcmAudioFrame::Format::SignedPadded24;
     constexpr PcmAudioFrame::Format outputFormat = PcmAudioFrame::Format::SignedPadded24;
     constexpr size_t parametricEqFilterCount = 2;
-    vector<double> frequencies{ 20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000 };
+    vector<double> frequencies{ 20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000,
+        1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000 };
     constexpr size_t soundLevelLength = 2;
     shared_ptr<AnalysisDispatcher> analysisDispatcher;
 
