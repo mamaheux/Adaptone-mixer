cb2d5841373825ac72c1bf84dbd7f1c145c34dcc
Marc-Antoine <maheux.marcantoine@gmail.com>
2019-09-03 18:24:34 -0400

    Correction de la compilation de fftw

--- source/3rdParty/make_fftw_unix.bash
+++ source/3rdParty/make_fftw_unix.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-f [ ! -d ./install ]; then
-    ./configure --enable-float --enable-type-prefix && make && export DESTDIR="${CMAKE_CURRENT_SOURCE_DIR}/fftw/install" && make install
+if [ ! -d ./install ]; then
+    ./configure --enable-float --enable-type-prefix && make && export DESTDIR="$PWD/install" && make install
 fi
