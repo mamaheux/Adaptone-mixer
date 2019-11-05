9f3f6bf46311b8bc0ed9d27fb398da74bdfd6d6d
alarochelle <dylarochelle@gmail.com>
2019-11-05 09:54:23 -0500

    Add basic Pyhton websocket communication for the probes demo

---
+++ tools/uniformization-demo/websocket.py
@@ -0,0 +1,26 @@
+import asyncio
+import websockets
+import json
+
+PROBE_LISTEN_MESSAGE_SEQ_ID = 25
+STOP_PROBE_LISTEN_MESSAGE_SEQ_ID = 26
+
+async def messageHandler(websocket, path):
+  while True:
+    async for data in websocket:
+      json_message = json.loads(data)
+
+      seq_id = json_message['seqId']
+
+      if seq_id == PROBE_LISTEN_MESSAGE_SEQ_ID:
+        # Handle listening to the corresponding probe
+        print(f"{json_message['seqId']}")
+        print(f"{json_message['data']['probeId']}")
+      elif seq_id == STOP_PROBE_LISTEN_MESSAGE_SEQ_ID:
+        # Stop listening to the corresponding probe
+        print(f"{json_message['seqId']}")
+
+start_websockets_server = websockets.serve(messageHandler, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_websockets_server)
+asyncio.get_event_loop().run_forever()
